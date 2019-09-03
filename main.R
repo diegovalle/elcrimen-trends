98b491f03111217a32a37cb4b80ac8e4377f9a7d
diego <git@diegovalle.net>
2019-09-03 13:50:19 -0500

    Update dplyr version when running in docker

--- main.R
+++ main.R
@@ -1,8 +1,12 @@
 ## This program does
 
+# tidybayes need dplyr >= 0.8.0
+if (packageVersion("dplyr") < "0.8.0")
+  update.packages("dplyr")
+
 ## Auto-Install packages
 .packs <- c("readr", "dplyr", "zoo", "ggplot2", "mgcv",
-            "lubridate", "stringr", "rconapo", "loo", "rstanarm",
+            "lubridate", "stringr", "loo", "rstanarm",
             "tidybayes", "jsonlite", "scales", "season", "directlabels",
             "hrbrthemes", "tidyr", "betareg", "bayesplot")
 .success <- suppressWarnings(sapply(.packs, require, character.only = TRUE))
@@ -10,6 +14,8 @@ if (length(names(.success)[!.success])) {
   install.packages(names(.success)[!.success])
   sapply(names(.success)[!.success], require, character.only = TRUE)
 }
+
+
 
 options(stringsAsFactors = FALSE)
 
